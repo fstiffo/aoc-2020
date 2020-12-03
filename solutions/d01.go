8b2b08c9cc20a54b5d6742bff970a3ecec5d5206
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-03 23:00:39 +0100

    getInput for day 1 added

--- solutions/d01.go
+++ solutions/d01.go
@@ -1,9 +1,20 @@
 package solutions
 
+import "strconv"
+
+func getInput(filename string) (input []int) {
+	lines := GetFileContent(filename)
+	input = make([]int, len(lines))
+	for i, s := range lines {
+		input[i], _ = strconv.Atoi(s)
+	}
+	return
+}
+
 // D01FirstHalf calculates the solution for the first half question of day 1
 func D01FirstHalf() int {
-	lines := GetFileContent("inputs/d01.txt")
-	return len(lines)
+	input := getInput("inputs/d01.txt")
+	return len(input)
 }
 
 // D01SecondHalf calculates the solution for the second half question of day 1
