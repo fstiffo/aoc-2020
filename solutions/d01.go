7b1604063893f5c988de4c84e989dfc0dd7d4391
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-04 09:24:00 +0100

    First day solution

--- solutions/d01.go
+++ solutions/d01.go
@@ -14,10 +14,30 @@ func getInput(filename string) (input []int) {
 // D01FirstHalf calculates the solution for the first half question of day 1
 func D01FirstHalf() int {
 	input := getInput("inputs/d01.txt")
-	return len(input)
+	size := len(input)
+	for first := 0; first < size; first++ {
+		for second := first; second < size; second++ {
+			if (input[first] + input[second]) == 2020 {
+				return input[first] * input[second]
+			}
+		}
+
+	}
+	return -1
 }
 
 // D01SecondHalf calculates the solution for the second half question of day 1
 func D01SecondHalf() int {
-	return 0
+	input := getInput("inputs/d01.txt")
+	size := len(input)
+	for first := 0; first < size; first++ {
+		for second := first + 1; second < size; second++ {
+			for third := second + 1; third < size; third++ {
+				if (input[first] + input[second] + input[third]) == 2020 {
+					return input[first] * input[second] * input[third]
+				}
+			}
+		}
+	}
+	return -1
 }
